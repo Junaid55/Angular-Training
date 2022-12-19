d1d6828768cae7888f330e0d3512bc943e4f4e4b
Junaid55 <77045046+Junaid55@users.noreply.github.com>
2022-12-19 12:27:18 +0530

    Add files via upload

---
+++ src-DirectiveTask/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'DirectiveTask';
+  clicked:boolean=false;
+  count:number=0;
+  arr:Array<number>=[];
+  constructor()
+  {
+    this.clicked=false;
+  }
+  btnClicked()
+  {
+    this.count++;
+    this.arr.push(this.count);
+    if(this.count%2==0)
+    {
+    this.clicked=false;
+    }
+    else
+    {
+      this.clicked=true;
+    }
+  }
+ 
+
+}
